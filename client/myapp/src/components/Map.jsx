49dee74a950fd67e489dc97ee3ca437832096090
AlessandroB96 <abelaj7196@gmail.com>
2022-05-21 13:39:25 -0400

    secure api key and added props for places

--- client/myapp/src/components/Map.jsx
+++ client/myapp/src/components/Map.jsx
@@ -1,35 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class Map extends Component {
-  static defaultProps = {
-    center: {
-      lat: 40.7812,
-      lng: -73.9665
-    },
-    zoom: 12
-  };
+function Map() {
 
-  render() {
+    const coordinates = {lat: 40.7812,lng: -73.9665};
+    const API_KEY = process.env.REACT_APP_MAPSAPI_KEY
+    
     return (
-      // Important! Always set the container height explicitly
-      <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: '' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+        <div className="map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={12}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
 }
 
 export default Map;
--- client/myapp/src/components/Places.jsx
+++ client/myapp/src/components/Places.jsx
@@ -1,25 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import { Autocomplete } from '@react-google-maps/api';
 import Map from './Map';
+import Card from './Card';
 
 const Places = () => {
-    return (
 
+    const [type, setType] = useState('hotels');
+    const [rating, setRating] = useState('all');
+
+    const places = [
+        {name: 'place1'},
+        {name: 'place2'},
+        {name: 'place3'},
+        {name: 'place4'},
+        {name: 'place5'},
+        {name: 'place6'},
+        {name: 'place7'},
+        {name: 'place8'},
+        {name: 'place9'},
+        {name: 'place10'},
+
+    ]
+
+    return (
         <div className="entire-places">
             <div className="places-container">
                 <div className="places-text">
-                    <p className="place-header">PLACES</p>
+                    <p className="place-header">Search for Hotels, Restuarants, and Attractions:</p>
                     <br />
-                    <input type="text" name="search" id="search" className="searchbox" placeholder="search for a city..." />
+                    {/* <input type="text" name="search" id="search" className="searchbox" placeholder="search for a city..." /> */}
+                    
+                    <br />
 
-                    <p className="recommended">RECOMMENDED SPOTS</p>
+                    <div className="type-container">
+                    <label className="type-title" htmlFor="types">Type: </label>
+                        <select name="all-types" id="type" value={type} onChange={(event) => setType(event.target.value)}>
+                            <option value="hotels">Hotels</option>
+                            <option value="restaurants">Restaurants</option>
+                            <option value="attractions">Attractions</option>
+                        </select>
+                    </div>
+
+                    <br />
+                    <div className="rating-container">
+                    <label className="rating-title" htmlFor="rating">Rating: </label>
+                        <select name="ratings" id="ratings" value={rating} onChange={(event) => setRating(event.target.value)}>
+                            <option value="all">All</option>
+                            <option value="plus3">+3.0</option>
+                            <option value="plus4">+4.0</option>
+                            <option value="plus4.5">+4.5</option>
+                        </select>
+                    </div>
+
+                    <div className="generated-places">
+                        {/* if there is no places, do not map array of values */}
+                        {places?.map((place) => (
+                            <div className="each-card">
+                                <Card place={place}/>
+                            </div>
+                        ))}
+                    </div>
+
+                    {/* <p className="recommended">RECOMMENDED SPOTS</p>
                     <ul>
                         <li>Spot 1</li>
                         <li>Spot 2</li>
                         <li>Spot 3</li>
                         <li>Spot 4</li>
                         <li>Spot 5</li>   
-                    </ul>
+                    </ul> */}
                 </div>
             </div>
             <div className="map-container">
