e6705d631ca4e032310e5ac117d307105df27965
Abbush <glgberat@yahoo.com>
2022-05-21 02:39:35 -0400

    USER model for backend done

--- client/myapp/src/components/Signup.js
+++ client/myapp/src/components/Signup.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
 
 const Signup = () => {
     
