0fe53cf18c96d55af1989c85f7134d307b6f930f
Abbush <glgberat@yahoo.com>
2022-05-22 18:37:04 -0400

    Places schema,typedef & resolvers

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,8 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-// import schema from Book.js
-//const bookSchema = require('./Book');
+// import schema from Place.js
+const placeSchema = require('./Place');
 
 const userSchema = new Schema(
   {
@@ -21,15 +21,15 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    // set savedBooks to be an array of data that adheres to the bookSchema
-    //savedBooks: [bookSchema],
+    // set savedBooks to be an array of data that adheres to the placeSchema
+    savedPlaces: [placeSchema],
   },
 //   set this to use virtual below
-//  {
-//    toJSON: {
-//      virtuals: true,
-//    },
-//   }
+ {
+   toJSON: {
+     virtuals: true,
+   },
+  }
 );
 
 // hash user password
@@ -47,10 +47,10 @@ userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
-//userSchema.virtual('bookCount').get(function () {
-//  return this.savedBooks.length;
-//});
+//when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+userSchema.virtual('placeCount').get(function () {
+ return this.savedPlaces.length;
+});
 
 const User = model('User', userSchema);
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -15,11 +15,35 @@ type Auth {
     me: User
   }
 
+  input InputPlace {
+    placeId: String
+    name: String
+    image: String
+    link: String
+    type: String
+    rating: Number
+  }
+
+  type Place {
+    placeId: ID!
+    name: String
+    image: String
+    link: String
+    Type: String
+    rating: Number
+  }
+
+
 type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    savePlace(newBook: InputPlace!): User
+    removePlace(placeId: ID!): User
 
   }
+
 `;
+
+
 
 module.exports = typeDefs;
