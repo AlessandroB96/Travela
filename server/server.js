857ada779047e4040ba37695098a32157fd302ce
Gravity <tevoned@gmail.com>
2022-05-22 23:36:43 -0400

    payment feature

--- server/server.js
+++ server/server.js
@@ -4,8 +4,44 @@ const path = require('path');
 const { typeDefs, resolvers } = require("./schemas");
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([[
+1, { priceInCents: 10000, name: "Travel Book" }],
+[2, { priceInCents: 20000, name: "Travel Guide" }
+],
+])
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.get(item.id)
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+    })
+    res.json({ url: session.url })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
+})
 
 const server = new ApolloServer({
   typeDefs,
